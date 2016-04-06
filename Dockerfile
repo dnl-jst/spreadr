39664dca8f30c3e0d49295951d7c7ac3ea996709
Daniel Jost <info@daniel-jost.de>
2016-04-06 06:51:38 +0200

    update spreadr to node 4.4 lts and http2 3.3.2

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
-FROM node:0.12-onbuild
+FROM node:4.4-onbuild
 EXPOSE 80
 EXPOSE 443
--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -1,5 +1,12 @@
+# install nodesource key
+curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
+
+# install repos
+echo 'deb https://deb.nodesource.com/node_4.x trusty main' > /etc/apt/sources.list.d/nodesource.list
+echo 'deb-src https://deb.nodesource.com/node_4.x trusty main' >> /etc/apt/sources.list.d/nodesource.list
+
 # update package lists
 apt-get update
-apt-get install -y nodejs npm
 
-ln -s /usr/bin/nodejs /usr/bin/node
+# install nodejs 4.x
+apt-get install -y nodejs
